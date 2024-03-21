5627fdcede058f8f2945d70d7a50ca5887d30f07
Diego Téllez Nieto <153697883+diegoTellezNieto@users.noreply.github.com>
2024-03-21 14:03:02 -0600

    ProgramWindow constructor and resize method creation

--- sistema.js
+++ sistema.js
@@ -11,7 +11,7 @@ class Size{
 }
 
 class Position{
-    constructor(X, Y){
+    constructor(X = 0, Y = 0){
         this.x = X;
         this.y = Y;
     }
@@ -19,5 +19,27 @@ class Position{
     move(newX, newY){
         this.x = newX;
         this.y = newY;
+    }
+}
+
+class ProgramWindow{
+    constructor (){
+        this.screenSize = new Size(800,600);
+        this.size = new Size();
+        this.position = new Position()
+    }
+
+    resizeProgramWindow(newSize){
+        let newWidth = Math.max(1, newSize.width);
+        let newHeight = Math.max(1, newSize.height);
+
+        const maxWidth = this.screenSize.width - this.position.x;
+        const maxHeight = this.screenSize.height - this.position.y;
+
+        newWidth = Math.min(newWidth, maxWidth);
+        newHeight = Math.min(newHeight, maxHeight);
+
+        this.size.width(newWidth);
+        this.size.height(newHeight);
     }
 }
